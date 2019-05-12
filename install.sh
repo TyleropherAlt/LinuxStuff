f667b48497a1bae1661b2edf71648002cec7baf1
Cat-Gaming <47046268+Cat-Gaming@users.noreply.github.com>
2019-05-12 10:35:10 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -12,4 +12,19 @@ sudo add-apt-repository ppa:graphics-drivers/ppa
 sudo apt update
 sudo apt install nvidia-driver-418 nvidia-settings
 sudo apt install libvulkan1 libvulkan1:i386
+
+sudo apt install tasksel
+sudo apt update
+sudo apt-get upgrade
+echo Do You Want to Install KDE (yes or no)?
+read input
+if [input == "yes"]
+then
+  sudo tasksel install kubuntu-desktop
+fi
+if [input == "no"]
+then
+  break
+fi
 echo Done..
+read d
